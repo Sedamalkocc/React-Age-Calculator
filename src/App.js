420f0f881bbb0f29b0866a3e6b08b64b7b61c59f
Seda Malkoç <sedaamalkocc@gmail.com>
2024-02-08 23:53:08 +0300

    age calculator ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const calculateAge = () => {
+    const birthdateValue = document.querySelector("#birthdate").value;
+    const result = document.querySelector("#result");
+
+    if (birthdateValue) {
+      const today = new Date();
+      const birthdate = new Date(birthdateValue);
+      let ageYears = today.getFullYear() - birthdate.getFullYear();
+      let ageMonth = today.getMonth() - birthdate.getMonth();
+      let ageDay = today.getDate() - birthdate.getDate();
+
+      if (ageDay < 0) {
+        ageMonth--;
+        ageDay += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+      }
+
+      if (ageMonth < 0) {
+        ageYears--;
+        ageMonth += 12;
+      }
+
+      result.innerText = `Your age is ${ageYears} years, ${ageMonth} months, and ${ageDay} days.`;
+      result.style.color = "#81689D";
+    } else {
+      result.innerText = "Please enter your date of birth!";
+      result.style.color = "red";
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Age Calculator</h1>
+      <label htmlFor="birthdate">Enter Your Birthdate: </label>
+      <input type="date" id="birthdate" />
+      <button onClick={calculateAge}>Calculate Age</button>
+      <p id="result"></p>
     </div>
   );
 }
