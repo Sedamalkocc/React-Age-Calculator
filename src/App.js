f5ef27785a99ff8f8a6ec3092a66c25c8b6b8bad
Seda Malkoç <sedaamalkocc@gmail.com>
2024-02-09 00:21:35 +0300

    css is done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
     if (birthdateValue) {
       const today = new Date();
       const birthdate = new Date(birthdateValue);
+
+      if (birthdate > today) {
+        result.innerText = "Please enter a valid birthdate!";
+        result.style.color = "red";
+        return;
+      }
+
       let ageYears = today.getFullYear() - birthdate.getFullYear();
       let ageMonth = today.getMonth() - birthdate.getMonth();
       let ageDay = today.getDate() - birthdate.getDate();
@@ -32,12 +39,14 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <h1>Age Calculator</h1>
-      <label htmlFor="birthdate">Enter Your Birthdate: </label>
-      <input type="date" id="birthdate" />
-      <button onClick={calculateAge}>Calculate Age</button>
-      <p id="result"></p>
+    <div className="App">
+      <div className="container">
+        <h1>Age Calculator</h1>
+        <label htmlFor="birthdate">Enter Your Birthdate: </label>
+        <input type="date" id="birthdate" />
+        <button onClick={calculateAge}>Calculate Age</button>
+        <p id="result"></p>
+      </div>
     </div>
   );
 }
